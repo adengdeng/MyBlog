f73e7ab495fee6aee9247ec1fbc2bb30a884b3fb
adengdeng <adengdeng@users.noreply.github.com>
2020-02-20 22:27:19 +0800

    add date

--- project/GoogleSheetAPITest-master/index.js
+++ project/GoogleSheetAPITest-master/index.js
@@ -1,22 +1,34 @@
 let sendButton = document.querySelector('button');
 
 function send() {
-  let name = document.querySelector('#nameValue').value;
-  let phone = document.querySelector('#phoneValue').value;
-  let demand = document.querySelector('#demandValue').value;
-  $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbyLPqsK6lueHYQHC0A0YjSEu_OBVnygdVSLe9yBFXBeyX6u8smI/exec",
-    data: {
-        "name": name,
-        "phone": phone,
-        "demand": demand
-    },
-    success: function(response) {
-      if(response == "成功"){
-        alert("成功");
-      }
-    },
-  });
+	var mydate = new Date();
+	let name = document.querySelector('#nameValue').value;
+	let phone = document.querySelector('#phoneValue').value;
+	let demand = document.querySelector('#demandValue').value;
+
+	$.ajax({
+		url: "https://script.google.com/macros/s/AKfycbyLPqsK6lueHYQHC0A0YjSEu_OBVnygdVSLe9yBFXBeyX6u8smI/exec",
+		data: {
+			"name": name,
+			"phone": phone,
+			"demand": demand,
+			"Date":mydate
+		},
+		success: function(response) {
+			if(response == "000"){
+			alert("簽到成功");
+			}
+		},
+		error: function(data) {
+			alert('簽到失敗'); 
+		}
+		});
 };
 
 sendButton.addEventListener('click', send);
+
+
+var loadingTimeout = window.setTimeout(function() {
+  alert('網路不穩定，請稍後再試');
+}, 10000); // 10000ms = 10sec
+
